bbc4a69cd3ef57959b58004238dce84f0c11b562
jose32117 <65979104+jose321171@users.noreply.github.com>
2026-03-15 20:56:44 -0600

    Update scan_char.asm

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -13,12 +13,15 @@ global scan_char
 scan_char:
 
     push ebp
-    mov ebp, esp
+    mov ebp, esp; Abrimos el stack frame
 
-    ; TODO:
-    ; 1. usar syscall read
-    ; 2. leer 1 byte desde stdin
-    ; 3. devolverlo en AL
+    mov eax, 3; Corresponde a syscall sys_read
+    mov ebx, 0; Corresponde a leer desde stdin es decir, el teclado
+    mov ecx, char_buffer; Direccion donde el kernl dejará el precioso byte.
+    mov edx, 1; leer exactamente 1 byte.
+    int 0x80; Llamamos al kernel
+
+    mov al, [char_buffer] ; Traemos el byte de memoria a AL
 
     mov esp, ebp
     pop ebp

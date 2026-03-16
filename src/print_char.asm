8ddc7bf99670481a559a7a229f3884b3f1143c21
jose32117 <65979104+jose321171@users.noreply.github.com>
2026-03-15 20:17:56 -0600

    Store character in memory before syscall write
    Implemented character storage in memory before printing.

--- src/print_char.asm
+++ src/print_char.asm
@@ -12,10 +12,23 @@ print_char:
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. Guardar el caracter en memoria
+    ; Obtener el caracter del parametro mov al, [ebp+8]
+    ; 1. Guardar el caracter en memoria (en la pila)
+
+    sub esp, 4; Quitamos 4 del stack pointer, que es lo mismo que reservar espacio, 4 bytes.
+    mov byte [esp], al; Guardamos el caracter en esa dirección
+
     ; 2. Usar syscall write
-    ; 3. Imprimir 1 byte
+    mov eax, 4; "Lo va a escribir"
+    mov ebx, 1;  "Escribe en pantalla"
+    mov ecx, esp; "Aquí está el caracter"
+    mov edx, 1; "Escribe 1 byte unicamente" sjjs.
+    
+    ; Ejecutamos el syscall
+    int 0x80; El SO se encarga de hacer lo que le pedimos. 
+
+    ; Limpiamos la pila
+    add esp, 4; Limpiamos los 4 bytes que reservamos en el paso 1.
 
     ; write(fd=1, buffer, 1)
 
